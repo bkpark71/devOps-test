d72e34bbc9e06b74ee6665ad7c3aa9321282e6bb
bkpark <bokyoungp@naver.com>
2024-03-20 17:51:13 +0900

    version12.1

--- src/day12/chapter11/HashMapTest.java
+++ src/day12/chapter11/HashMapTest.java
@@ -1,13 +1,21 @@
 package day12.chapter11;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapTest {
   public static void main(String[] args) {
-    HashMap<String, String> animals = new HashMap<>();
-    animals.put("호랑이", "tiger");
-    animals.put("표범", "leopard");
-    animals.put("사자", "lion");
+//    HashMap<String, String> animals = new HashMap<>();
+//    animals.put("호랑이", "tiger");
+//    animals.put("표범", "leopard");
+//    animals.put("사자", "lion");
+
+    Map<String, String> map = Map.of(
+            "호랑이", "tiger",
+            "표범", "leopard",
+            "사자", "lion");
+
+    HashMap<String, String> animals = new HashMap<>(map);
 
     System.out.println(animals);
     animals.replaceAll((k,v) -> v.toUpperCase());
